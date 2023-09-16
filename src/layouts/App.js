a5edc8fda44dba7236291ec2d60fecf4ce84a5b7
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-16 19:54:08 +0200

    added components

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import '../layouts/App';
 import '../styles/App.css';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
-    return <div className='App-header'>App</div>;
+    return (
+      <div className='App'>
+        <header>{<Header />}</header>
+        <main>
+          <aside>{<Navigation />}</aside>
+          <section>{<Page />}</section>
+        </main>
+        <footer>{<Footer />}</footer>
+      </div>
+    );
   }
 }
 
