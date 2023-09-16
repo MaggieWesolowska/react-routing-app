f5680add9b8826a2420e90a82ffcec38af76c944
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-16 16:34:51 +0200

    init commit

---
+++ src/layouts/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../layouts/App';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return <div className='App-header'>App</div>;
+  }
+}
+
+export default App;
