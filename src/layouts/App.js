0fb6414bd8c8ed2cd18804cc63851d0f07d80078
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-16 20:07:50 +0200

    edited styles for page

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -9,7 +9,7 @@ import Footer from './Footer';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className='app'>
         <header>{<Header />}</header>
         <main>
           <aside>{<Navigation />}</aside>
