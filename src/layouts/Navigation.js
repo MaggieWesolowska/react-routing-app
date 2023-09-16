352b0a3c3e7d74a6df3653cce850a15478242bf9
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-16 20:41:36 +0200

    added navigation and background images

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/Navigation.css';
 
-const Nav = () => {
-  return <div>Navigation</div>;
+const list = [
+  { name: 'main page', path: '/' },
+  { name: 'products', path: '/products' },
+  { name: 'contact', path: '/contact' },
+  { name: 'user admin', path: '/admin' },
+];
+
+const Navigation = () => {
+  const menu = list.map(item => (
+    <li key={item.name}>
+      <NavLink
+        to={item.path}
+        // exact={item.exact ? item.exact : false}
+      >
+        {item.name}
+      </NavLink>
+    </li>
+  ));
+  return (
+    <nav className='nav'>
+      <ul>
+        {menu}
+        {/* <li>
+          <NavLink to='/' exact=true >Main Page</NavLink>
+        </li>
+        <li>
+          <NavLink to='products'>Products</NavLink>
+        </li>
+        <li>
+          <NavLink to='contact'>Contact Us</NavLink>
+        </li>
+        <li>
+          <NavLink to='admin'>User Admin</NavLink>
+        </li> */}
+      </ul>
+    </nav>
+  );
 };
 
-export default Nav;
+export default Navigation;
