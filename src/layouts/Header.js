f452cec00652c5c118a4475b8821e2b8f041978c
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-17 15:49:03 +0200

    added header and background imgs

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import img1 from '../images/header1.jpg';
+import img2 from '../images/header2.jpg';
+import img3 from '../images/header3.jpg';
+import img4 from '../images/header4.jpg';
+import '../styles/Header.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  //to choose background randomly:
+  // const images = [img1, img2, img3];
+  // const index = Math.floor(Math.random() * 3);
+  // const img = images[index];
+  return (
+    <>
+      <Routes>
+        <Route
+          path='/'
+          element={<img src={img1} alt='city1' />}></Route>
+        <Route
+          path='/products'
+          element={<img src={img2} alt='city2' />}></Route>
+        <Route
+          path='/contact'
+          element={<img src={img3} alt='city3' />}></Route>
+        <Route
+          path='/admin'
+          element={<img src={img4} alt='lake' />}></Route>
+        <Route
+          path='/*'
+          element={<img src={img4} alt='lake' />}></Route>
+      </Routes>
+      {/* <img src={img} alt='header-img' /> */}
+    </>
+  );
 };
 
 export default Header;
