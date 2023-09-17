b3800589c91a6ccc41a35832a712dd8d24422035
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-17 17:13:20 +0200

    added contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../styles/ContactPage.css';
 
-const ContactPage = () => {
-  return <div>Contact Us</div>;
-};
+class ContactPage extends Component {
+  state = {
+    value: '',
+    isEmpty: true,
+  };
+
+  handleSubmit = ev => {
+    ev.preventDefault();
+    this.setState({
+      value: '',
+      isEmpty: true,
+    });
+  };
+
+  handleChange = ev => {
+    if (ev.target.value.length > 0) {
+      this.setState({
+        value: ev.target.value,
+        isEmpty: false,
+      });
+    } else {
+      this.setState({
+        value: ev.target.value,
+        isEmpty: true,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className='contact'>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Contact Us!</h4>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder='Enter your comments..'></textarea>
+          <button>Send</button>
+        </form>
+        {/* {!this.state.isEmpty
+          ? alert('Do you really want to leave this page?')
+          : null} */}
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
