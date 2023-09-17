4333027f05c12e3e973d9da30d4e74c24b70fe15
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-17 16:09:42 +0200

    edited components and added routing to pages

---
+++ src/layouts/Page.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import ProductPage from '../pages/ProductPage';
+import ContactPage from '../pages/ContactPage';
+import AdminPage from '../pages/AdminPage';
+import ErrorPage from '../pages/ErrorPage';
+
+const Page = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+      <Route
+        path='/products'
+        element={<ProductPage />}></Route>
+      <Route
+        path='/contact'
+        element={<ContactPage />}></Route>
+      <Route path='/admin' element={<AdminPage />}></Route>
+      <Route path='/*' element={<ErrorPage />}></Route>
+    </Routes>
+  );
+};
+
+export default Page;
